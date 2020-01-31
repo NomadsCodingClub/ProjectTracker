04390ec9f9eca0d24a5114f4683af139a0d2900c
Spencer Thomas <fifa59@hotmail.co.uk>
2020-01-31 21:26:05 +0000

    Added Data project and included first 2 entities

---
+++ ProjectTracker.Data/Models/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectTracker.Data.Models
+{
+    public class Project
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public User CreatedBy { get; set; }
+
+    }
+}
---
+++ ProjectTracker.Data/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectTracker.Data.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Forename { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+}
