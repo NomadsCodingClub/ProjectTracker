c49e99cb247fb58d9283cf60aefa82e863579efa
xL30N <l30n@l30n.co.uk>
2020-02-02 09:52:48 +0000

    Added task and status entities

---
+++ ProjectTracker.Data/Models/Status.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectTracker.Data.Models
+{
+    public class Status
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ProjectTracker.Data/Models/Task.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectTracker.Data.Models
+{
+    public class Task
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public User Owner { get; set; }
+        public DateTime DueDate { get; set; }
+        public Status Status { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public User CreatedBy { get; set; }
+
+    }
+}
