089e2c8703f29a17995394b4c66e87da1ea04315
xL30N <l30n@l30n.co.uk>
2020-02-23 14:37:37 +0000

    Added model classes to the db context

--- ProjectTracker/Data/ApplicationDbContext.cs
+++ ProjectTracker/Data/ApplicationDbContext.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ProjectTracker.Data.Models;
 
 namespace ProjectTracker.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
