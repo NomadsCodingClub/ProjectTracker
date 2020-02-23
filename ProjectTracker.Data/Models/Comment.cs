7912fee3a692869287e3ffee6fbf4f7935b9e743
xL30N <l30n@l30n.co.uk>
2020-02-23 13:04:16 +0000

    Added comment to the model

---
+++ ProjectTracker.Data/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ProjectTracker.Data.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        [Display(Name = "Posted On")]
+        public DateTime PostedOn { get; set; }
+        public virtual User User { get; set; }
+        public virtual Task Task { get; set; }
+    }
+}
