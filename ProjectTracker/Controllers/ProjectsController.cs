4f47dabd451b53ab0640c44c2db7629028f3fc59
xL30N <l30n@l30n.co.uk>
2020-02-23 15:49:08 +0000

    Added table of projects to the view and updated the view imports to include the namespace ProjectTracker.Data.Models

--- ProjectTracker/Controllers/ProjectsController.cs
+++ ProjectTracker/Controllers/ProjectsController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProjectTracker.Data;
+using ProjectTracker.Data.Models;
 
 namespace ProjectTracker.Controllers
 {
     public class ProjectsController : Controller
     {
+        //TODO: Implement respository 
+        private readonly ApplicationDbContext Db;
+
+        public ProjectsController(ApplicationDbContext db)
+        {
+            Db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IQueryable<Project> projects = Db.Projects.OrderBy(p => p.Name);
+
+            return View(projects);
         }
     }
 }
